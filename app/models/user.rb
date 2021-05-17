15828c7ce1fe6eddd5d3b73c8d61bd875859faec
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-05-17 21:28:54 +0200

    added mailing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :attendances, foreign_key: 'attendee_id', class_name: 'Attendance'
   has_many :events, through: :attendances
   has_many :admin_events, foreign_key: 'admin_id', class_name: "Event"
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
