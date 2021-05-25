be478389083e186fe29a68c03a226307739524be
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-05-25 13:03:43 +0200

    added password to user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 
 User.destroy_all
 10.times do |i|
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Name.first_name+"@yopmail.com", encrypted_password: Faker::Crypto.sha1, reset_password_token: Faker::Crypto.sha1)
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Name.first_name+"@yopmail.com", encrypted_password: Faker::Crypto.sha1, reset_password_token: Faker::Crypto.sha1, password: "mdpmdp")
   puts "#{i+1} users created"
 end
 
 Event.destroy_all
 10.times do |i|
-  Event.create!(start_date: Faker::Date.between(from: '2021-09-23', to: '2021-11-25'), duration: 60, title: Faker::Science.scientist, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.within(range: 1..1000), location: Faker::Tea.variety, admin: User.all.sample)
+  Event.create!(start_date: Faker::Date.between(from: '2021-09-23', to: '2021-11-25'), duration: 60, title: Faker::Games::Pokemon.name, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.within(range: 1..1000), location: Faker::Tea.variety, admin: User.all.sample)
   puts "#{i+1} events created"
 end
 
